dc5447f9e115f61e19789ca3bf3c2737796f1f1d
Javier Sanchez <javiersanchez0021@gmail.com>
2023-01-21 19:32:01 -0300

    Se agrego otro archivo

---
+++ tipos_de_datos.js
@@ -0,0 +1,17 @@
+// Tipos de datos en JS
+// String
+// Boolean
+// Null
+// Number
+// Undefined
+// Objecto
+
+// Definicion de variables
+// var
+// let
+// const
+
+// var miPrimeraVariable = "Primera variable"
+
+let miPrimeraVariable = "Mi primera variable"
+console.log(miPrimeraVariable)
